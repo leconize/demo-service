fe4aee173727a5c6c79707084111c2cd95dee75b
Leconize <g.supavit@gmail.com>
2017-04-04 12:02:34 +0700

    add update query

--- order-service/src/main/java/demo/adapter/UserAdapter.java
+++ order-service/src/main/java/demo/adapter/UserAdapter.java
@@ -3,10 +3,12 @@ package demo.adapter;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.cache.annotation.Cacheable;
 
 @Service
 public class UserAdapter {
 
+    @Cacheable("users")
     public User getUserDetail(long userId) {
         RestTemplate restTemplate = new RestTemplate();
         String url = "http://localhost:9001/user/" + userId;
